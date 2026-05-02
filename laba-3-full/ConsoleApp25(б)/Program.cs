4cca3fae649b99831499b3dc1c933f48e1778e3a
Rail <164221657+railakhmerov@users.noreply.github.com>
2026-05-02 11:27:05 +0400

    Add files via upload

---
+++ laba-3-full/ConsoleApp25(б)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp25_б_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
