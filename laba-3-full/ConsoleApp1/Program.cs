c571a53dab7a8e1d6efe25085a9776663f6bf67f
Rail <164221657+railakhmerov@users.noreply.github.com>
2026-05-02 11:31:13 +0400

    Add files via upload

---
+++ laba-3-full/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите A (true/false): ");
+            bool A = bool.Parse(Console.ReadLine());
+
+            Console.Write("Введите B (true/false): ");
+            bool B = bool.Parse(Console.ReadLine());
+
+            bool a = !(!A && !B) && A;
+            bool b = !(!A || !B) || A;
+            bool c = !(!A || !B) && B;
+
+            Console.WriteLine($"а) {a}");
+            Console.WriteLine($"б) {b}");
+            Console.WriteLine($"в) {c}");
+        }
+    }
+}
---
+++ laba-3-full/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool A = true, B = false, C = true;
+
+            Console.WriteLine(!(A || (!B && C)));
+            Console.WriteLine(A && !(B && A || !C));
+            Console.WriteLine(!(!A || B && C));
+            Console.WriteLine(!(A || !B && C) || C);
+            Console.WriteLine(!(A && !B || C) && B);
+            Console.WriteLine(!(!A || B && C) || A);
+        }
+    }
+}
