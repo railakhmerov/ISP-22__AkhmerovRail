c1ad9d75b1f563a002009d1cb3fee1b8ec36f6f9
Rail <164221657+railakhmerov@users.noreply.github.com>
2026-05-02 12:12:06 +0400

    Add files via upload

---
+++ laboratory-4/ConsoleApp1.3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
