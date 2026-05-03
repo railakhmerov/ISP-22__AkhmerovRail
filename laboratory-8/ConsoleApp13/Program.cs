22c3e2d6624171a309e31083b1fe0774e6f43129
Rail <164221657+railakhmerov@users.noreply.github.com>
2026-05-03 12:08:17 +0400

    Add files via upload

---
+++ laboratory-8/ConsoleApp13/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("file1.txt") || !File.Exists("file2.txt"))
+            {
+                Console.WriteLine("Файлы не найдены");
+                return;
+            }
+
+            string text1 = File.ReadAllText("file1.txt");
+            string text2 = File.ReadAllText("file2.txt");
+
+            File.WriteAllText("result.txt", text1 + text2);
+            Console.WriteLine("Выполнено");
+        }
+    }
+}
